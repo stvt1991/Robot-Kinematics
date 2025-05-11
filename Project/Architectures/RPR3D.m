80cba7cc34291e56d0a3fa6438c3c4198e8a5457
Swaminath Venkateswaran <45877040+stvt1991@users.noreply.github.com>
2025-05-11 14:37:40 +0200

    Update RPR3D.m

--- Project/Architectures/RPR3D.m
+++ Project/Architectures/RPR3D.m
@@ -1,4 +1,5 @@
 function RPR3D(th1,rho,th2)
+%% Creator: Swaminath Venkateswaran, ESILV Engineering School, Paris, France %%
 %% A function named RPR3D used to generate the DH model of the RPR robot in 3D space %%
 % INPUT(S):  th1, th2: The three revolute angles
 %            rho: The prismatic link 
